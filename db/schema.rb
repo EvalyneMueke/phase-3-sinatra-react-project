7567fcdaf44eb76dec502af0eb7ebe57f6aeaa7c
Evalyne <evalyne.mueke@student.moringaschool.com>
2023-03-02 09:17:16 +0300

    Added users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_02_060306) do
+ActiveRecord::Schema.define(version: 2023_03_02_061333) do
 
   create_table "categories", force: :cascade do |t|
     t.string "category"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2023_03_02_060306) do
     t.string "importance"
     t.integer "category_id"
     t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
