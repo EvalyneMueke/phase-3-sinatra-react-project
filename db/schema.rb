542e362591eea148f4d6ac1729105cbdc69af90d
Evalyne <evalyne.mueke@student.moringaschool.com>
2023-03-02 09:07:03 +0300

    Added category table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_02_055422) do
+ActiveRecord::Schema.define(version: 2023_03_02_060306) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "category"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "item"
